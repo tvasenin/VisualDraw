9b66777386b4e92a7e483400c6c9624713dfb424
Timofey Vasenin <timofey.vasenin@gmail.com>
2012-10-30 13:27:56 +0700

    Add more usable shapes
    v0.2

--- VisualDraw/Form1.Designer.cs
+++ VisualDraw/Form1.Designer.cs
@@ -28,23 +28,83 @@ namespace VisualDraw
         /// </summary>
         private void InitializeComponent()
         {
+            this.radioButton_Cross = new System.Windows.Forms.RadioButton();
+            this.radioButton_Line = new System.Windows.Forms.RadioButton();
+            this.MainCanvas = new System.Windows.Forms.Panel();
+            this.radioButton_Circle = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
+            // 
+            // radioButton_Cross
+            // 
+            this.radioButton_Cross.AutoSize = true;
+            this.radioButton_Cross.Location = new System.Drawing.Point(12, 389);
+            this.radioButton_Cross.Name = "radioButton_Cross";
+            this.radioButton_Cross.Size = new System.Drawing.Size(51, 17);
+            this.radioButton_Cross.TabIndex = 0;
+            this.radioButton_Cross.TabStop = true;
+            this.radioButton_Cross.Text = "Cross";
+            this.radioButton_Cross.UseVisualStyleBackColor = true;
+            this.radioButton_Cross.CheckedChanged += new System.EventHandler(this.MainScreen_radioButtons_CheckedChanged);
+            // 
+            // radioButton_Line
+            // 
+            this.radioButton_Line.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.radioButton_Line.AutoSize = true;
+            this.radioButton_Line.Location = new System.Drawing.Point(12, 413);
+            this.radioButton_Line.Name = "radioButton_Line";
+            this.radioButton_Line.Size = new System.Drawing.Size(45, 17);
+            this.radioButton_Line.TabIndex = 1;
+            this.radioButton_Line.TabStop = true;
+            this.radioButton_Line.Text = "Line";
+            this.radioButton_Line.UseVisualStyleBackColor = true;
+            this.radioButton_Line.CheckedChanged += new System.EventHandler(this.MainScreen_radioButtons_CheckedChanged);
+            // 
+            // MainCanvas
+            // 
+            this.MainCanvas.BackColor = System.Drawing.Color.White;
+            this.MainCanvas.Location = new System.Drawing.Point(69, 12);
+            this.MainCanvas.Name = "MainCanvas";
+            this.MainCanvas.Size = new System.Drawing.Size(611, 442);
+            this.MainCanvas.TabIndex = 2;
+            this.MainCanvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainCanvas_MouseDown);
+            this.MainCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.MainCanvas_Paint);
+            // 
+            // radioButton_Circle
+            // 
+            this.radioButton_Circle.AutoSize = true;
+            this.radioButton_Circle.Location = new System.Drawing.Point(12, 437);
+            this.radioButton_Circle.Name = "radioButton_Circle";
+            this.radioButton_Circle.Size = new System.Drawing.Size(51, 17);
+            this.radioButton_Circle.TabIndex = 3;
+            this.radioButton_Circle.TabStop = true;
+            this.radioButton_Circle.Text = "Circle";
+            this.radioButton_Circle.UseVisualStyleBackColor = true;
+            this.radioButton_Circle.CheckedChanged += new System.EventHandler(this.MainScreen_radioButtons_CheckedChanged);
             // 
             // MainScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(692, 466);
+            this.Controls.Add(this.radioButton_Circle);
+            this.Controls.Add(this.MainCanvas);
+            this.Controls.Add(this.radioButton_Line);
+            this.Controls.Add(this.radioButton_Cross);
             this.MaximizeBox = false;
             this.Name = "MainScreen";
             this.Text = "VisualDraw";
-            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainScreen_Paint);
-            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainScreen_MouseDown);
+            this.Load += new System.EventHandler(this.MainScreen_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton radioButton_Cross;
+        private System.Windows.Forms.RadioButton radioButton_Line;
+        private System.Windows.Forms.Panel MainCanvas;
+        private System.Windows.Forms.RadioButton radioButton_Circle;
 
     }
 }
--- VisualDraw/Form1.cs
+++ VisualDraw/Form1.cs
@@ -10,42 +10,146 @@ namespace VisualDraw
 {
     public partial class MainScreen : Form
     {
-        List<Cross> Points = new List<Cross>();
+        List<Figure> Figures = new List<Figure>();
+        bool IsFigureStart = true;
+        Point FigureStart;
+               
         
         public MainScreen()
         {
             InitializeComponent();
         }
 
-        private void MainScreen_MouseDown(object sender, MouseEventArgs e)
+        private void MainScreen_radioButtons_CheckedChanged(object sender, EventArgs e)
+        {
+            IsFigureStart = true;
+        }
+
+
+        private void MainCanvas_MouseDown(object sender, MouseEventArgs e)
         {
             if (e.Button != MouseButtons.Left) return;
             this.Text = Convert.ToString(e.X) + " - " + Convert.ToString(e.Y);
-            Points.Add(new Cross(e.X, e.Y));
-            Invalidate();
+            if (radioButton_Cross.Checked)
+            {
+                Figures.Add(new Cross(e.X, e.Y));
+            }
+            else if (radioButton_Line.Checked)
+            {
+                if (IsFigureStart)
+                {
+                    FigureStart = new Point(e.X, e.Y);
+                    IsFigureStart = false;
+                }
+                else
+                {
+                    Figures.Add(new Line(FigureStart, new Point(e.X, e.Y)));
+                    IsFigureStart = true;
+                }
+            }
+            else if (radioButton_Circle.Checked)
+            {
+                if (IsFigureStart)
+                {
+                    FigureStart = new Point(e.X, e.Y);
+                    IsFigureStart = false;
+                }
+                else
+                {
+                    Figures.Add(new Circle(new Point(FigureStart.X, FigureStart.Y), (float) Math.Sqrt(Math.Pow(e.X-FigureStart.X, 2) + Math.Pow(e.Y-FigureStart.Y, 2))));
+                    IsFigureStart = true;
+                }
+            }
+
+
+            MainCanvas.Invalidate();
         }
 
-        private void MainScreen_Paint(object sender, PaintEventArgs e)
+        private void MainCanvas_Paint(object sender, PaintEventArgs e)
         {
-            foreach (Cross p in this.Points)
+            foreach (Figure p in this.Figures)
             {
                 p.DrawWith(e.Graphics);
             }
         }
-    }
-    public class Cross
-    {
-        int X, Y;
-        Pen p = new Pen(Color.Black);
-        public Cross(int X, int Y)
+
+        private void MainScreen_Load(object sender, EventArgs e)
         {
-            this.X = X; this.Y = Y;
+            //Taxes: Remote Desktop Connection and painting
+            //http://blogs.msdn.com/oldnewthing/archive/2006/01/03/508694.aspx
+            if (System.Windows.Forms.SystemInformation.TerminalServerSession)
+                return;
+
+            System.Reflection.PropertyInfo aProp =
+                typeof(System.Windows.Forms.Control).GetProperty(
+                    "DoubleBuffered",
+                    System.Reflection.BindingFlags.NonPublic |
+                    System.Reflection.BindingFlags.Instance);
+
+            aProp.SetValue(MainCanvas, true, null);
         }
 
-        public void DrawWith(Graphics g)
+
+        
+        
+    }
+
+    public abstract class Figure
+    {
+        protected Pen p = new Pen(Color.Black);
+        
+        public abstract void DrawWith(Graphics g);
+    }
+    public class Line: Figure
+    {
+        Point S, F;
+
+        public Line(Point s, Point f)
         {
-            g.DrawLine(p, X - 2, Y - 2, X + 3, Y + 3);
-            g.DrawLine(p, X - 2, Y + 2, X + 3, Y - 3);
+            this.S = s;
+            this.F = f;
+        }
+        public override void DrawWith(Graphics g)
+        {
+            g.DrawLine(p, S.X, S.Y, F.X, F.Y);
+            //new Cross(S);
+        }
+
+    }
+    public class Cross : Figure
+    {
+        Point C;
+
+        public Cross(Point p)
+        {
+            this.C = p;
+        }
+
+        public Cross(int x, int y)
+            :this(new Point(x, y))
+        { }
+        
+        public override void DrawWith(Graphics g)
+        {
+            g.DrawLine(p, C.X - 2, C.Y - 2, C.X + 2, C.Y + 2);
+            g.DrawLine(p, C.X - 2, C.Y + 2, C.X + 2, C.Y - 2);
         }
     }
+    public class Circle : Figure
+    {    
+        Point C;
+        float R;
+
+        public Circle(Point c, float r)
+        {
+            this.C = c;
+            this.R = r;
+        }
+        public override void DrawWith(Graphics g)
+        {
+            g.DrawEllipse(p, C.X - R, C.Y - R, 2 * R, 2 * R);
+        }
+
+    }
+
 }
